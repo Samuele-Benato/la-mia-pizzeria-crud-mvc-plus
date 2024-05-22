044e42a3fb2aeeea06b844c75fccf7fb583fb76c
Samuele-Benato <samuele.benato@gmail.com>
2024-05-22 17:30:38 +0200

    fix : hide asp-page="/Account/Manage/Index"

--- la-mia-pizzeria-static/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ la-mia-pizzeria-static/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,7 +80,7 @@ namespace la_mia_pizzeria_static.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Ricorda password?")]
             public bool RememberMe { get; set; }
         }
 
